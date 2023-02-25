a660225fab476b77626643bd1306435b827090dc
harsh123-baba <harshitgoyal213@gmail.com>
2023-02-25 19:42:29 +0530

    done one more quyestion

--- patterns/10_special_right_angle.cpp
+++ patterns/10_special_right_angle.cpp
@@ -1,17 +1,20 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
     int n;
     cin>>n;
     int i = 1;
+
     while(i<=n){
         int j = 1;
-
+        int val=i;
         while(j<=i){
-            cout<<"* ";
-            j += 1;
+            cout<<val<<" ";
+            val++;
+            j++;
         }
-        i+=1;
+        i++;
         cout<<endl;
-    }   
+    }
 }
