3fefedccb58a2f2340faa1e1301815923d172503
harsh123-baba <harshitgoyal213@gmail.com>
2023-03-04 18:34:40 +0530

    dont take this commit serious

--- patterns/26_dabang_pattern.cpp
+++ patterns/26_dabang_pattern.cpp
@@ -1,27 +1,52 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int row = 1;
+//     while(row<=n){
+//         int col = 1;
+//         while(col <= n-row+1){
+//             cout<<col;
+//             col++;
+//         }
+//         int val = 2*row-2;
+//         while(val){
+//             cout<<"*";
+//             val--;
+//         }
+//         int col2 = n-row+1;
+//         while(col2){
+//             cout<<col2;
+//             col2--;
+//         }
+//         row++;
+//         cout<<endl;
+        
+//     }
+// }
+
+// 123321 
+// 12**21
+// 1****1
+// 
+// 
+
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     cin>>n;
-    int row = 1;
-    while(row<=n){
-        int col = 1;
-        while(col <= n-row+1){
-            cout<<col;
-            col++;
-        }
-        int val = 2*row-2;
-        while(val){
-            cout<<"*";
-            val--;
-        }
-        int col2 = n-row+1;
-        while(col2){
-            cout<<col2;
-            col2--;
-        }
-        row++;
-        cout<<endl;
+    int i = 0;
+    while(i<n){
+        int j = 0;
+        // first angle
+        //second tringle
+        // third angle
         
+
+
+        i++;
+
     }
 }
